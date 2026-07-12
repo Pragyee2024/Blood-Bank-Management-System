3291fc5b210b8f3315b5c524d64e94a5e6a3fa48
shresthaukti <shrestha5738u@gmail.com>
2026-07-12 14:50:10 +0545

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -5,5 +5,5 @@ define('DB_PORT', 5432);
 define('DB_NAME', 'blood_bank');
 define('DB_USER', 'postgres');
 define('DB_PASS', '');   // postgres password while pushing use git.ignore
-// this is for reference and after keeping pwd save it to device only
+// this is for reference and after keeping pwd save it as config.php in your device to run
 ?>
